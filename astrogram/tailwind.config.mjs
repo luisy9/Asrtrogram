fbac201182dfc68031c28dfdf71ad5dcd45079ff
luisy9 <luisdeharo4@gmail.com>
2024-03-22 14:29:29 +0100

    styles login

--- astrogram/tailwind.config.mjs
+++ astrogram/tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Varela", "sans-serif"],
+      // Aquí puedes agregar otras variantes de fuente si las tienes
+    }
+  },
+  plugins: [],
+};
